d774d4d1f739ddbcdb38fabe2de135cbed41f66d
aadv1k <aadv1k@outlook.com>
2023-12-16 12:36:28 +0530

    Finish implementation for day07, part01

--- day07/part01.go
+++ day07/part01.go
@@ -1,9 +1,13 @@
+// Credit goes here: I ended up implementing the logic of this
+// https://github.com/AleUP170/AdventOfCode/blob/main/Day07/01.py
+
 package day07
 
 import (
-	"log"
-	"strings"
-	"github.com/aadv1k/AdventOfGo2023/utils"
+	"fmt"
+	"regexp"
+	"sort"
+	"strconv"
 )
 
 const (
@@ -16,73 +20,115 @@ const (
 	FiveKind
 )
 
-var Strengths = []byte{'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'}
-
-func indexOf(element byte, array []byte) int {
-	for i := 0; i < len(array); i++ {
-		if array[i] == element {
-			return i
-		}
-	}
-	return -1
+type Hand struct {
+	cards string
+	bid   int
 }
 
-func CompareHandsGT(hand1, hand2 string) bool {
-	for i := range hand1 {
-		if indexOf(hand1[i], Strengths) >= indexOf(hand2[i], Strengths) {
-			return true
-		}
-	}
-	return false
+type RankedHand struct {
+	hand Hand
+	rank int
 }
 
-func GetHandType(hand string) int {
-	letters := make(map[rune]int)
-	frequency := make([]int, 6)
+var matches = [][]Hand{
+	{}, {}, {}, {}, {}, {}, {},
+}
 
-	for i := range hand {
-		letters[rune(hand[i])]++
+func findMatches(hands []Hand) {
+	for _, hand := range hands {
+		count := make(map[rune]int)
+
+		for _, card := range hand.cards {
+			count[card]++
+		}
+
+		value := 1
+		for _, c := range count {
+			value *= c
+		}
+
+		switch value {
+		case 1:
+			matches[6] = append(matches[6], hand)
+		case 2:
+			matches[5] = append(matches[5], hand)
+		case 3:
+			matches[3] = append(matches[3], hand)
+		case 4:
+			if len(count) == 2 {
+				matches[1] = append(matches[1], hand)
+			} else {
+				matches[4] = append(matches[4], hand)
+			}
+		case 5:
+			matches[0] = append(matches[0], hand)
+		case 6:
+			matches[2] = append(matches[2], hand)
+		default:
+			fmt.Println("oh no")
+		}
+	}
+}
+
+func convertAndOrderMatches() []RankedHand {
+	var convertedMatches []RankedHand
+
+	for _, category := range matches {
+		var temp []RankedHand
+
+		for _, hand := range category {
+			// Substituting T J Q K A with A B C D E for ordering later
+			cards := regexp.MustCompile(`A`).ReplaceAllString(hand.cards, "E")
+			cards = regexp.MustCompile(`T`).ReplaceAllString(cards, "A")
+			cards = regexp.MustCompile(`J`).ReplaceAllString(cards, "B")
+			cards = regexp.MustCompile(`Q`).ReplaceAllString(cards, "C")
+			cards = regexp.MustCompile(`K`).ReplaceAllString(cards, "D")
+
+			num, err := strconv.ParseInt(cards, 16, 0)
+			if err != nil {
+				fmt.Println("Error converting cards to number:", err)
+				continue
+			}
+
+			temp = append(temp, RankedHand{hand: hand, rank: int(num)})
+		}
+
+		sort.Slice(temp, func(i, j int) bool {
+			return temp[i].rank > temp[j].rank
+		})
+
+		convertedMatches = append(convertedMatches, temp...)
 	}
 
-	for i := range letters {
-		frequency[letters[i]]++
-	}
-
-	if frequency[5] == 1 { // the number 5 appeared once
-		return FiveKind
-	} else if frequency[4] == 1 && frequency[1] == 1 { // the number 4 appeared once, and 1 once
-		return FourKind
-	} else if frequency[2] == 1 && frequency[3] == 1 {
-		return FullHouse
-	} else if frequency[3] == 1 && frequency[1] == 2 {
-		return ThreeKind
-	} else if frequency[2] == 2 {
-		return TwoPair
-	} else {
-		return HighCard
-	}
+	return convertedMatches
 }
 
 func Part01(input string) {
-	lines := strings.Split(input, "\n")
+	lines := input
+	hands := make([]Hand, 0)
 
-	var hands []string
-	winnings := make(map[string]int)
+	re := regexp.MustCompile(`[\dAKQJT]+`)
+	bidRe := regexp.MustCompile(` [\d]+`)
 
-	for _, line := range lines {
+	for _, line := range regexp.MustCompile(`\n`).Split(lines, -1) {
 		if len(line) == 0 {
 			continue
 		}
 
-		word := strings.Fields(line)
+		cards := re.FindString(line)
+		bid, _ := strconv.Atoi(bidRe.FindString(line)[1:])
 
-		hands = append(hands, word[0])
-		winnings[word[0]] = utils.ParseInt(word[1])
-
-		rank := GetHandType(word[0])
-		
-		log.Printf("%s -> %d", word[0], rank)
+		hands = append(hands, Hand{cards: cards, bid: bid})
 	}
 
-	_ = hands
+	findMatches(hands)
+
+	convertedMatches := convertAndOrderMatches()
+
+	total := 0
+	for i := 0; i < len(convertedMatches); i++ {
+		total += convertedMatches[i].hand.bid * (len(convertedMatches) - i)
+	}
+
+	fmt.Println(total)
 }
